7039b42d591068e5238add46e3af80188a8d7401
PrzemyslawMatlega <przemekmat@yahoo.com>
2019-12-14 10:24:59 +0100

    Spinner and sorting grid of posts

--- src/components/Viewer/TheViewer.vue
+++ src/components/Viewer/TheViewer.vue
@@ -1,174 +1,203 @@
 <template>
   <div>
-  
-    <TheViewerGrid 
-      :allPosts="allPosts"
-      @hasPicked="updateIndex"
-      @getPostsList="getPostsList"
-    />
+    <div class="click" style="cursor:pointer" @click="getPostsList">
+      <h1>CLICK ME</h1>
+    </div>
+    <transition name="fade">
+      <div class="gridLoader" v-if="gridLoad">
+        <GridLoader></GridLoader>
+      </div>
+      <TheViewerGrid
+        v-else
+        :allPosts="allPosts"
+        @hasPicked="updateIndex"
+        @getPostsList="getPostsList"
+      />
+    </transition>
 
     <transition name="fade">
-        <TheViewerPopup 
+      <TheViewerPopup
         v-if="showPopup"
-        :currentPost="allPosts[currentIndex]" 
+        :key="currentIndex"
+        :currentPost="allPosts[currentIndex]"
         :currentIndex="currentIndexValidation"
-        @closePopup="showPopup  = !showPopup" 
+        @closePopup="showPopup  = !showPopup"
         @changeSlide="currentIndex += $event"
-        @delateCurentPost="delateCurentPost" />
+        @delateCurentPost="delateCurentPost"
+      />
     </transition>
 
     <router-view>
-    <transition name="fade">
-      <TheViewerUpload 
-        v-if="isUploadPopupOn"
-        @switchPopup="$emit('closeUpload')" />
-    </transition>
-
+      <transition name="fade">
+        <TheViewerUpload v-if="isUploadPopupOn" @switchPopup="$emit('closeUpload')" />
+      </transition>
     </router-view>
-
-
   </div>
 </template>
 <script>
-  import firebase from '../../myFireBase.js'
-  import TheViewerGrid from './TheViewerGrid'
-  import TheViewerPopup from './TheViewerPopup'
-  import TheViewerUpload from './TheViewerUpload'
- 
-  export default {
-    props:{
-      isUploadPopupOn: Boolean,
-    },
-    data() {
-      return {
-        storageRef: Function,
-        showPopup: false,
-        allPosts: [],
-        currentIndex: 0,
-        allPostsNameList: [],
-      }
-    },
-    methods: {
-      delateCurentPost() {
-    
-        //1. Delate post from list on database
-        const postToDelete = this.allPosts[this.currentIndex].name;
-        firebase.database().ref(`/posts/${postToDelete}`).remove()
+import firebase from "../../myFireBase.js";
+import TheViewerGrid from "./TheViewerGrid";
+import TheViewerPopup from "./TheViewerPopup";
+import TheViewerUpload from "./TheViewerUpload";
+import GridLoader from "vue-spinner/src/GridLoader.vue";
 
-        //2. Delete img from storage
-        this.storageRef.child(`slide_viewer_imgs/${postToDelete}`).delete().then().catch(error => console.log(error))
-      
-        //3. Delete from current view and change for next
-        this.showPopup = false;
-        this.allPosts.splice(this.currentIndex, 1)
+export default {
+  props: {
+    isUploadPopupOn: Boolean
+  },
+  data() {
+    return {
+      storageRef: Function,
+      showPopup: false,
+      allPosts: [],
+      currentIndex: 0,
+      allPostsNameList: [],
+      gridLoad: false
+    };
+  },
+  methods: {
+    delateCurentPost() {
+      //1. Delate post from list on database
+      const postToDelete = this.allPosts[this.currentIndex].name;
+      firebase
+        .database()
+        .ref(`/posts/${postToDelete}`)
+        .remove();
 
-        if(this.allPosts.length >0){
-          if(this.currentIndex != 0){
-            this.currentIndex = this.currentIndex -1 
-          }
-          this.showPopup = true;
+      //2. Delete img from storage
+      this.storageRef
+        .child(`slide_viewer_imgs/${postToDelete}`)
+        .delete()
+        .then()
+        .catch(error => console.log(error));
+
+      //3. Delete from current view and change for next
+      this.showPopup = false;
+      this.allPosts.splice(this.currentIndex, 1);
+
+      if (this.allPosts.length > 0) {
+        if (this.currentIndex != 0) {
+          this.currentIndex = this.currentIndex - 1;
         }
-
-
-      },
-      getImage(newPosts) {
-    
-        newPosts.forEach(singlePost => {
-
-          this.allPostsNameList.push(singlePost.imgName);
-          const listRef = this.storageRef.child('slide_viewer_imgs/' + singlePost.imgName);
-          
-          listRef.getDownloadURL().then(url => this.allPosts.push({
-              ...singlePost,
-              url: url,
-            }))
-            .catch(error => {
-              switch (error.code) {
-                case 'storage/object-not-found':
-                  break;
-
-                case 'storage/unauthorized':
-                  break;
-
-                case 'storage/canceled':
-                  break;
-
-                case 'storage/unknown':
-                  break;
-              }
-            });
-        })
-
-      },
-      checkForUpdates(incomingPosts) {
-        // Check for new posts
-        let newPosts = incomingPosts.filter( incomingPost => !this.allPostsNameList.includes(incomingPost.imgName))
-        this.getImage(newPosts)
-
-        // Check for delated posts 
-        let delatedPosts = this.allPosts.filter(delatedPost => !incomingPosts.includes(delatedPost))
-        //to do    
-
-        this.incomingPosts = []
-
-      },
-      getPostsList() {
-        firebase.database().ref('/posts/').once('value').then((snapshot) => {
-          const postsFromDB = snapshot.val();
-          const incomingPosts = [];
-
-          for (const item in postsFromDB){
-              incomingPosts.push(postsFromDB[item])
-          }
-
-          this.checkForUpdates(incomingPosts);
-        }, (error) => console.log(error))
-
-      },
-      updateIndex(index){
-        this.currentIndex = index;
         this.showPopup = true;
       }
-
     },
+    getImage(newPosts) {
+      this.gridLoad = true;
+      newPosts.forEach(singlePost => {
+        this.allPostsNameList.push(singlePost.imgName);
+        const listRef = this.storageRef.child(
+          "slide_viewer_imgs/" + singlePost.imgName
+        );
 
-    computed: {
-      currentIndexValidation() {
-        if (this.currentIndex == 0) {
-          return 'First'
-        } else if (this.currentIndex == (this.allPosts.length - 1)) {
-          return 'Last'
-        } else {
-          return 'Middle'
-        }
-      }
+        listRef
+          .getDownloadURL()
+          .then(url =>
+            this.allPosts.push({
+              ...singlePost,
+              url: url
+            })
+          )
+          .catch(error => {
+            switch (error.code) {
+              case "storage/object-not-found":
+                break;
+
+              case "storage/unauthorized":
+                break;
+
+              case "storage/canceled":
+                break;
+
+              case "storage/unknown":
+                break;
+            }
+          });
+      });
+      setTimeout(() => {
+        this.gridLoad = false;
+      }, 1000);
     },
+    checkForUpdates(incomingPosts) {
+      // Check for new posts
+      let newPosts = incomingPosts.filter(
+        incomingPost => !this.allPostsNameList.includes(incomingPost.imgName)
+      );
+      this.getImage(newPosts);
 
-    created() {
-      this.storageRef = firebase.storage().ref();
+      // Check for delated posts
+      let delatedPosts = this.allPosts.filter(
+        delatedPost => !incomingPosts.includes(delatedPost)
+      );
+      //to do
+
+      this.incomingPosts = [];
     },
+    getPostsList() {
+      firebase
+        .database()
+        .ref("/posts/")
+        .once("value")
+        .then(
+          snapshot => {
+            const postsFromDB = snapshot.val();
+            const incomingPosts = [];
 
-    components: {
-      TheViewerUpload,
-      TheViewerPopup,
-      TheViewerGrid,
-      
+            for (const item in postsFromDB) {
+              incomingPosts.push(postsFromDB[item]);
+            }
+
+            this.checkForUpdates(incomingPosts);
+          },
+          error => console.log(error)
+        );
+    },
+    updateIndex(index) {
+      this.currentIndex = index;
+      this.showPopup = true;
     }
-  }
+  },
 
+  computed: {
+    currentIndexValidation() {
+      if (this.currentIndex == 0) {
+        return "First";
+      } else if (this.currentIndex == this.allPosts.length - 1) {
+        return "Last";
+      } else {
+        return "Middle";
+      }
+    }
+  },
+
+  created() {
+    this.storageRef = firebase.storage().ref();
+  },
+  mounted() {},
+
+  components: {
+    TheViewerUpload,
+    TheViewerPopup,
+    TheViewerGrid,
+    GridLoader
+  }
+};
 </script>
 
 
 <style lang="scss">
-  .viewerGrid {
-    width: 70%;
-    margin: 5rem auto;
-    display: grid;
-    grid-template-columns: repeat(3, 30rem);
-    grid-auto-rows: 30rem;
-    grid-column-gap: 4rem;
-    grid-row-gap: 4rem;
-
-  }
-
+.viewerGrid {
+  width: 70%;
+  margin: 5rem auto;
+  display: grid;
+  grid-template-columns: repeat(3, 30rem);
+  grid-auto-rows: 30rem;
+  grid-column-gap: 4rem;
+  grid-row-gap: 4rem;
+}
+.gridLoader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
 </style>
--- src/components/Viewer/TheViewerGrid.vue
+++ src/components/Viewer/TheViewerGrid.vue
@@ -1,38 +1,31 @@
 <template>
   <div>
-    <div class="click" style="cursor:pointer" @click="$emit('getPostsList')">
-      <h1>CLICK ME</h1>
-    </div>
     <div class="viewerGrid">
-
-     
-      <TheViewerGridPost 
-      v-for="(single, index) in allPosts" 
-      :key="single.name"
-      :id="single.name" 
-      :url="single.url"
-      @click.native="$emit('hasPicked', index)"
-       />
-
+      <TheViewerGridPost
+        v-for="(single, index) in allPosts"
+        :key="single.name"
+        :id="single.name"
+        :url="single.url"
+        @click.native="$emit('hasPicked', index)"
+      />
     </div>
-
   </div>
 </template>
 
 
 <script>
- import TheViewerGridPost from './TheViewerGridPost'
+import TheViewerGridPost from "./TheViewerGridPost";
 
-  export default {
-      props : {
-          allPosts : Array,
-      },
-      components: {
-        TheViewerGridPost,
-      },
-      methods: {
-        
-      },
+export default {
+  props: {
+    allPosts: Array
+  },
+  components: {
+    TheViewerGridPost
+  },
+  methods: {},
+  created() {
+    this.allPosts.sort((a, b) =>  parseFloat(b.createdAt) - parseFloat(a.createdAt));
   }
-
+};
 </script>
